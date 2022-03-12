d5e2018e0880431c0d8dc5154e6f7abf62c4d689
Alberth <alberthbernarth@gmail.com>
2022-03-12 19:35:01 -0400

    comunicación entre componentes

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
+import PropTypes from 'prop-types';
 
-export const AddCategory = () => {
+export const AddCategory = ( { setCategories } ) => {
 
-    const [inputValue, setInputValue] = useState('Hola Mundo');
+    const [inputValue, setInputValue] = useState('');
 
     const handleInputChange = ( e ) => {
         setInputValue( e.target.value );
@@ -10,8 +11,11 @@ export const AddCategory = () => {
 
     const handleSubmit = ( e ) => {
         e.preventDefault();
-        console.log('submit hecho');
 
+        if ( inputValue.trim().length > 2) {
+            setCategories( cats => [...cats, inputValue] );
+            setInputValue('');
+        }
     }
 
     return (
@@ -24,3 +28,7 @@ export const AddCategory = () => {
         </form>
     );
 };
+
+AddCategory.propTypes = {
+    setCategories: PropTypes.func.isRequired
+}
